6044abbf6118d3b707a3a3977028e0b5e68ffc2e
Mark Ryk <marco.look@outlook.com>
2021-08-24 17:38:06 -0300

    adicionando links do font awesome, bootstrap e google fonts à pagina inicial

--- index.php
+++ index.php
@@ -31,14 +31,29 @@
 								<a href="gerador_get_set/index.php" class="btn btn-dark color"> Gerador de Get e Set </a>&nbsp;
 							</div>
 							<div class="col">
-								<a href="https://color.adobe.com/pt/create/color-wheel" class="btn btn-dark color" target="_blank"> Adobe Color </a>&nbsp;
+								<a href="funcoes_data_e_hora/index.php" class="btn btn-dark color"> Manipular data e hora </a>&nbsp;
 							</div>
 						</div>
 						<br>
 						<div class="row">
 							<div class="col">
-								<a href="funcoes_data_e_hora/index.php" class="btn btn-dark color"> Manipular data e hora </a>&nbsp;
+								<a href="https://color.adobe.com/pt/create/color-wheel" class="btn btn-dark color" target="_blank"> Adobe Color </a>&nbsp;
 							</div>
+							<div class="col">
+								<a href="https://fontawesome.com/" class="btn btn-dark color" target="_blank"> Font Awesome </a>&nbsp;
+							</div>
+						</div>
+						<br>
+						<div class="row">
+							<div class="col">
+								<a href="https://getbootstrap.com/" class="btn btn-dark color" target="_blank"> Bootstrap </a>&nbsp;
+							</div>
+							<div class="col">
+								<a href="https://fonts.google.com/" class="btn btn-dark color" target="_blank"> Google Fonts </a>&nbsp;
+							</div>
+						</div>
+						<br>
+						<div class="row">
 							<!--<div class="col">
 								<a href="#" class="btn btn-dark color"> Calculadora (em breve) </a>&nbsp;
 							</div>-->
