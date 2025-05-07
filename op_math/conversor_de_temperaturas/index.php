b9a507d722f4c98fa08b53d883bf44638c639df6
Mark Ryk <marco.look@outlook.com>
2025-05-07 19:59:24 -0300

    iniciada feature conversor de temperaturas

--- op_math/conversor_de_temperaturas/index.php
+++ op_math/conversor_de_temperaturas/index.php
@@ -1,45 +1,40 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<!--Css p/ Bootstrap -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-	<link rel="stylesheet" type="text/css" href="../style.css">
-	<title> Operações matemáticas </title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<title> Conversor de temperaturas </title>
 </head>
 <body>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-1"></div>
-			<div class="col-md-9">
-				<div class="jumbotron" style="background-color: #F738B1;">
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<div class="jumbotron" style="background-color: #F6736F;">
 					<ul class="nav nav-pills justify-content-end">
 					  <li class="nav-item">
 					    <a class="nav-link btn btn-outline-dark" href="../index.php"> Início </a>
 					  </li>
 					</ul>
-					<h1 class="display-4"> Operações matemáticas </h1>					
-					<!--<p class="lead"> Xxx </p>-->
-					<hr class="my-4"><br>
-					<div class="container">
-						<div class="row">
-							<div class="col">
-								<a href="conversor_de_bases/index.php" class="btn btn-dark color flex-sm-fill text-sm-center"> Conversor de bases numéricas </a>&nbsp;
-							</div>
-							<div class="col">
-								<a href="conversor_de_temperaturas/index.php" class="btn btn-dark color"> Conversor de temperaturas </a>&nbsp;
-							</div>
-						</div>
-					</div>
-				<a href="https://github.com/markryk" class="MarkRyk" target="_blank"> MarkRyk </a>				
+					<h1 class="display-4"> Conversor de temperaturas </h1>
+					<h2> Página em construção </h2>
+					<hr class="my-4">
+					<!--<p class="lead"> Clique em gerar </p>-->
+										
+					<br>
+					<a href="../"><button class="btn btn-dark btn-lg color"> Voltar </button></a>
+					<a style="left: 500px;" href="https://github.com/markryk" class="MarkRyk" target="_blank"> MarkRyk </a>
+				</div>				
 			</div>
-			<div class="col-md-1"></div>
+			<div class="col-md-2"></div>
 		</div>
 	</div>
-	
 	<!-- JS p/ Bootstrap (os 3, nessa ordem; esse trecho deve vir antes de quaisquer outros scripts)-->
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+	<script type="text/javascript" src="js/jquery-1.2.6.pack.js"></script>
+	<script type="text/javascript" src="js/jquery-maskedinput-1.1.4.pack.js"/></script>
 </body>
 </html>
